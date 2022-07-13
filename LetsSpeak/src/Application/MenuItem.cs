fbe4263fec2c212a6526d9ce6fa1c935fe7b27ed
Fabio Yamashita <yamashitafabio@gmail.com>
2022-07-13 14:15:39 -0300

    Update - Project is working

---
+++ LetsSpeak/src/Application/MenuItem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sharprompt;
+
+namespace LetsSpeak
+{
+    public class MenuItem
+    {
+        public MenuType Type { get; }
+        private static MenuItem _root;
+        private MenuItem parent = null;
+        private string title;
+        private List<MenuItem> items;
+        private Action action;
+
+        public MenuItem(string title)
+        {
+            this.title = title;
+            items = new List<MenuItem>();
+            if (_root == null) _root = this;
+            Type = MenuType.Submenu;
+        }
+
+        public MenuItem(string title, Action action) : this(title)
+        {
+            this.action = action;
+            Type = MenuType.Command;
+        }
+
+        public void Add(MenuItem menuItem)
+        {
+            menuItem.parent = this;
+            items.Add(menuItem);
+        }
+
+        public void Execute()
+        {
+            Console.Clear();
+            Console.ResetColor();
+
+            var menuItems = Prompt.Select(title, items);
+
+            switch (menuItems.Type)
+            {
+                case MenuType.Submenu:
+                    menuItems.Execute();
+                    break;
+
+                case MenuType.Command:
+                    menuItems.action();
+                    Console.ReadKey(true);
+                    break;
+
+                default:
+                    break;
+            }
+
+            menuItems.parent.Execute();
+        }
+
+        public override string ToString()
+        {
+            return title;
+        }
+
+    }
+}
---
+++ LetsSpeak/src/Program.cs
@@ -0,0 +1,30 @@
+﻿using Sharprompt;
+
+namespace LetsSpeak
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PromptConfig.DefaultConfig();
+            Console.Title = "Let's Speak";
+
+            EnglishDictionary dict = new EnglishDictionary();
+
+            var mainMenu = new MenuItem("Main Menu");
+            mainMenu.Add(new MenuItem("Register new word", dict.AddWordToDatabase));
+            mainMenu.Add(new MenuItem("Search word meaning", dict.SearchWord));
+            mainMenu.Add(new MenuItem("EXIT", () => Environment.Exit(0)));
+
+            mainMenu.Execute();
+            
+            //Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            //dictionary.Add("ball", "bola");
+            //dictionary.Add("speak", "falar");
+            //dictionary.Add("test pear", "sweet fruit");
+            //dictionary.Add("peacock", "pavão");
+            //dictionary.Add("spear", "lança");
+
+        }
+    }
+}
